f86f9c2aebe5ff25ca20c4649d7ee61f24337e1b
srmalsallout <62013676+srmalsallout@users.noreply.github.com>
2023-01-31 23:24:25 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Saferoad.Protocol.Astro;
+namespace AstroTestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var samplePacketBytes = TestConst.locBytes;
+            TestHeader(samplePacketBytes);
+            TestBody(samplePacketBytes);
+
+            var samplePacketBatchBytes = TestConst.BchBytes;
+            TestHeader(samplePacketBatchBytes);
+            TestBody(samplePacketBatchBytes);
+
+            Console.WriteLine("Test Completed");
+        }
+
+        static void TestHeader(byte[] RawPacketBytes)
+        {
+            (MessageIDs MsgId, string IMEI, byte[] Response) = Astro500.ParseHeader(RawPacketBytes);
+            Console.WriteLine($"Test Header: MsgId: {MsgId}, IMEI: {IMEI}");
+        }
+
+        static void TestBody(byte[] RawPacketBytes)
+        {
+            (MessageIDs MsgId, string IMEI, byte[] Response, List<Astro500Location> Locations) = Astro500.ParseBody(RawPacketBytes);
+            Console.WriteLine($"Test Body: MsgId: {MsgId}, IMEI: {IMEI}, LocationCounts: {Locations.Count}");
+            foreach (var loc in Locations)
+            {
+                Console.WriteLine($"Test Body - Loc: {loc.RecordDateTime}, Speed:{loc.Speed}, Lat:{loc.Latitude}, Long:{loc.Longitude}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# Create your project
+Use your termainal to create new project, <BR>
+You can pull this github repo directly instead
+
+```Terminal
+dotnet new console -o "AstroTestProj"
+```
+
+# Add Astro latest Package Reference to your Project
+```XML
+<ItemGroup>
+    <PackageReference Include="Saferoad.Protocol.Astro" Version="1.0.6" />
+</ItemGroup>
+```
+Then Restore your project as follow
+```Terminal
+dotnet restore
+```
+
+# Call the Library and include it in your code
+```C#
+using Saferoad.Protocol.Astro;
+namespace AstroTestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            .
+            .
+            .
+            (MessageIDs MsgId, string IMEI, byte[] Response) = Astro500.ParseHeader(TestConst.locBytes);
+            Console.WriteLine($"Test Header: MsgId: {MsgId}, IMEI: {IMEI}");
+            .
+            .
+            .
+            
+```
+
+# Run The Sampe Project
+
+Then run your project execute the command in terminal
+```Terminal
+dotnet run
+```
+
+
+# Example and Result
+
+<img src=".\test.png" alt="testing" width="800"/>
